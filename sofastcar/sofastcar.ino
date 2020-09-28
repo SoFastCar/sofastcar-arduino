2571a653cb073ac4acb706f2172951e6322ebcc9
devyhan93 <devyhan93@gmail.com>
2020-09-28 17:21:33 +0900

    feat: bluetooth print serial monitor
    - 아이폰에서 블루투스를 통해 보낸 문자 시리얼 모니터로 확인 완료

--- sofastcar/sofastcar.ino
+++ sofastcar/sofastcar.ino
@@ -1,9 +1,26 @@
+#include<SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2, 3);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  BTSerial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(BTSerial.available()){
+    char bt;
+    bt = BTSerial.read();
+    switch (bt) {
+      case 'a':
+        Serial.write(bt);
+        break;  
+      case 'b':
+        Serial.write(bt);
+        break;
+      default:
+        break;
+    }
+  }
 }
